71dab1f51de37335a78919872b44d69c53665ca4
Zhasmina Stoyanova <sjasmian1607@gmail.com>
2026-04-30 01:35:32 +0200

    openblas fix attempt C

--- src/R_fit_levenberg.cpp
+++ src/R_fit_levenberg.cpp
@@ -11,10 +11,21 @@
 #include <omp.h>
 #endif
 
+// OPENBLAS attempt
+extern "C" {
+    int openblas_get_num_threads(void) __attribute__((weak));
+    void openblas_set_num_threads(int num_threads) __attribute__((weak));
+}
+
 SEXP fit_levenberg (SEXP y, SEXP offset, SEXP disp, SEXP weights, SEXP design,
                     SEXP beta, SEXP tol, SEXP maxit, SEXP lambda_reg, SEXP alpha_reg,
                     SEXP num_threads) {
     BEGIN_RCPP
+
+    // OPENBLAS attempt
+    if (openblas_get_num_threads) {
+    	Rprintf("Inside C++, OpenBLAS is using %d threads.\n", openblas_get_num_threads()); 
+    }
 
     any_numeric_matrix counts(y);
     const int num_tags=counts.get_nrow();
@@ -54,6 +65,12 @@ SEXP fit_levenberg (SEXP y, SEXP offset, SEXP disp, SEXP weights, SEXP design,
     //PARALLEL
     #ifdef _OPENMP
         if (n_threads > 0) {
+
+           if (openblas_set_num_threads) {
+           	openblas_set_num_threads(1);
+           	Rprintf("UPDATE: OpenBLAS is using %d threads after the command.\n", openblas_get_num_threads());
+          }           
+
           omp_set_num_threads(n_threads);
           //std::cout << "Max threads available: " << omp_get_max_threads() << std::endl;
           Rprintf("Max threads available: %d\n", omp_get_max_threads());
